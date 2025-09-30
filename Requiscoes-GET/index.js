d46025b55b7d3c9b19ef20053b1415416b2ec41a
Maycke <reydsonk@gmail.com>
2025-09-29 21:55:11 -0300

    Iniciando criacao de arquivos

---
+++ Requiscoes-GET/index.js
@@ -0,0 +1,7 @@
+/* API (Application Programming Interface) é um conjunto de regras, protocolos e ferramentas para a construção de software e aplicações. 
+Ela define como diferentes partes do software devem interagir.
+Em termos mais simples, é um contrato que permite que um sistema (sua aplicação, por exemplo) converse e troque informações com outro sistema 
+(como o sistema de um banco, uma rede social, um serviço de mapas, etc.) de forma padronizada.
+Exemplo prático: Quando você usa um aplicativo de previsão do tempo no seu celular, ele não tem um satélite próprio para coletar dados. 
+Ele usa uma API de um serviço meteorológico (como o OpenWeatherMap, por exemplo) para solicitar a temperatura e as condições atuais, 
+e o serviço retorna esses dados para o seu aplicativo exibir. */
